8d2eb3760a1860a91cdc67e701b08f3a47a3a497
Wagner Martins <wagnermartins410@gmail.com>
2021-02-01 10:10:29 -0300

    Product insert, update, delete

--- backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/ProductResource.java
@@ -26,19 +26,15 @@ import com.devsuperior.dscatalog.services.ProductService;
 public class ProductResource {
 	
 	@Autowired
-	private ProductService service;
+	private ProductService service; 
 	
 	@GetMapping
 	public ResponseEntity<Page<ProductDTO>> findAll(
 			@RequestParam(value = "page", defaultValue = "0") Integer page,
 			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
 			@RequestParam(value = "direction", defaultValue = "ASC") String direction,
-			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-			
-			){
-		
-		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction),orderBy);
-		
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy){		
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage, Direction.valueOf(direction),orderBy);		
 		Page<ProductDTO> list = service.findAllPaged(pageRequest);
 		return ResponseEntity.ok().body(list);		
 	}
